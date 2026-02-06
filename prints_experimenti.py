e8b6a491a7146f2608e16a41ddd9c6ca94ecd46a
Reinis Furmanis <Reinisfurmanis@gmail.com>
2026-02-06 23:44:58 +0200

    Added markdown file

---
+++ prints_experimenti.py
@@ -0,0 +1,11 @@
+print("Eksperiments 1: Sveiki!")
+print("Eksperiments 2: 2 + 3 =", 2 + 3)
+
+vards = "Reinis"
+print("Eksperiments 3: Mans vārds ir", vards)
+
+a = 10
+b = 4
+print("Eksperiments 4: a - b =", a - b)
+
+print("Eksperiments 5: Teksts ar jaunrindu\njāunā rinda")
